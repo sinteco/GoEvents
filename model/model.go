d3cb36c92432cc4c48446c225d604330076fc896
sinteco <centg2g@gmail.com>
2023-04-12 21:47:33 +0300

    model updated

--- model/model.go
+++ model/model.go
@@ -8,4 +8,13 @@ type Event struct {
  Id primitive.ObjectID
  Title string
  Article string
+ Detail string
+ EventDateFrom string
+ EventDateTo string
+ EventOrganizer string
+ EventLocation string
+ EventImage string
+ CreatedBy int
+ CreatedDate string
+ UpdatedDate string
 }
